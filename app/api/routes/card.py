355704b3583fe66b03113b8cd38721c978197d24
alex <staleksandr05@gmail.com>
2025-07-06 22:38:39 +0300

    Fixed swagger docs

--- app/api/routes/card.py
+++ app/api/routes/card.py
@@ -1,10 +1,8 @@
 from typing import Annotated
-
 from fastapi import APIRouter, Depends, Path
 from app.enums import CardStatus
 from app.utils import handle_business_errors
 from app.schemas import (
-    SuccessSchema,
     CardPU,
     CardSchema,
     OwnerCardSchema,
@@ -13,10 +11,10 @@ from app.schemas import (
 )
 from app.dependencies import CardServiceDep, AdminDep, CurrentUserDep
 
-router = APIRouter(prefix="/api/cards", tags=["Cards (Admin)"])
+router = APIRouter(prefix="/api/cards", tags=["Cards"])
 
 
-@router.get("/", summary="Get card by query")
+@router.get("/", summary="Get list of cards by query. For admin all cards, own for user")
 @handle_business_errors
 def get_all_cards(
         user: CurrentUserDep,
@@ -26,43 +24,43 @@ def get_all_cards(
     return service.get(user, schema)
 
 
-@router.post("/", status_code=201, summary="Add card")
+@router.post("/", status_code=201, summary="Create card. For admin only")
 @handle_business_errors
 def add_card(_: AdminDep, service: CardServiceDep, schema: CardCreate) -> CardSchema:
     return service.add(schema)
 
 
-@router.get("/{id}", summary="Get card by id")
+@router.get("/{id}", summary="Get card by id. For admin all cards, own for user")
 @handle_business_errors
 def get_all_cards(user: CurrentUserDep, service: CardServiceDep, id: int) -> list[OwnerCardSchema]:
     return service.get_by_id(user, id)
 
 
-@router.delete("/{id}", status_code=204, summary="Delete card")
+@router.delete("/{id}", status_code=204, summary="Delete card. For admin only")
 @handle_business_errors
 def delete_card(_: AdminDep, service: CardServiceDep, id: int):
     return service.delete(id)
 
 
-@router.put("/{id}", summary="Replace card")
+@router.put("/{id}", summary="Replace card. For admin only")
 @handle_business_errors
 def replace_card(_: AdminDep, service: CardServiceDep, id: int, schema: CardReplace) -> CardSchema:
     return service.replace(id, schema)
 
 
-@router.patch("/{id}", summary="Part update card")
+@router.patch("/{id}", summary="Update card. For admin only")
 @handle_business_errors
 def part_update_card(_: AdminDep, service: CardServiceDep, id: int, schema: CardPU) -> CardSchema:
     return service.part_update(id, schema)
 
 
-@router.patch("/{id}/status", summary="Change card status")
+@router.patch("/{id}/status", summary="Change card status. For admin only")
 @handle_business_errors
 def block_card(_: AdminDep, service: CardServiceDep, id: int, new_status: CardStatus) -> OwnerCardSchema:
     return service.change_status(id, new_status)
 
 
-@router.post("/{id}/transfer", summary="Transfer money from card to other card of authed user")
+@router.post("/{id}/transfer", summary="Transfer money from one user card to another")
 @handle_business_errors
 def transfer_money(
         id: Annotated[int, Path(ge=1)],
--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -9,40 +9,40 @@ from app.schemas import (
 )
 from app.dependencies import UserServiceDep, AdminDep
 
-router = APIRouter(prefix="/api/users", tags=["Users (only admin)"])
+router = APIRouter(prefix="/api/users", tags=["Users"])
 
 
-@router.get("/", summary="Get list of users by query")
+@router.get("/", summary="Get list of users by query. For admin only")
 @handle_business_errors
 def get_user(_: AdminDep, service: UserServiceDep, schema: UserFilter = Depends()) -> list[UserSchema]:
     return service.get(schema)
 
 
-@router.post("/", status_code=201, summary="Create user")
+@router.post("/", status_code=201, summary="Create user. For admin only")
 @handle_business_errors
 def add_user(_: AdminDep, service: UserServiceDep, schema: UserCreate) -> UserSchema:
     return service.add(schema)
 
 
-@router.get("/{id}", summary="Get user by id")
+@router.get("/{id}", summary="Get user by id. For admin only")
 @handle_business_errors
 def get_user(_: AdminDep, service: UserServiceDep, id: int) -> UserSchema:
     return service.get_by_id(id)
 
 
-@router.delete("/{id}", status_code=204, summary="Delete user")
+@router.delete("/{id}", status_code=204, summary="Delete user. For admin only")
 @handle_business_errors
 def delete_user(_: AdminDep, service: UserServiceDep, id: int):
     return service.delete(id)
 
 
-@router.put("/{id}", summary="Replace user")
+@router.put("/{id}", summary="Replace user. For admin only")
 @handle_business_errors
 def replace_user(_: AdminDep, service: UserServiceDep, id: int, schema: UserReplace) -> UserSchema:
     return service.replace(id, schema)
 
 
-@router.patch("/{id}", summary="Part update user")
+@router.patch("/{id}", summary="Update user. For admin only")
 @handle_business_errors
 def add_user(_: AdminDep, service: UserServiceDep, id: int, schema: UserPU) -> UserSchema:
     return service.part_update(id, schema)
